ae46c88218f4561b233452e6ca6a2ac142483fd2
Satoshi Konno <skonno@cybergarage.org>
2021-11-14 00:30:40 +0900

    Update version

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ from setuptools import setup
 
 setup(
     name='uecho',
-    version='0.1',
-    description="Time series failure analisys utility.",
+    version='0.8.0',
+    description="uEcho for Python is a portable development framework for ECHONET Lite developers.",
     author='Satoshi Konno',
     author_email='skonno@cybergarage.org',
-    url='https://ghe.corp.yahoo.co.jp/nosql/foreman-py',
+    url='https://github.com/cybergarage/uecho-py.git',
     packages=[
         'uecho'
     ],
