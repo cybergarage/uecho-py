8c76a1ab46579622ff3dda6a5699c9de7c25d636
Satoshi Konno <skonno@cybergarage.org>
2021-11-14 01:27:21 +0900

    Fix Bytes::from_int() for Python 2.7

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: System :: Networking',
         'License :: OSI Approved :: Apache Software License',
     ],
