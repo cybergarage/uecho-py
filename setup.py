e01507f87856ed14cdc27e4522d2878a2a42691d
Satoshi Konno <skonno@cybergarage.org>
2021-11-14 12:29:14 +0900

    Update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name='uecho',
-    version='0.8.1',
+    version='0.8.2',
     description="uEcho for Python is a portable development framework for ECHONET Lite developers.",
     long_description=long_description,
     long_description_content_type='text/markdown',
