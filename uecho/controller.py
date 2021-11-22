23ff311215d944c4b68a8e5ee7563183495f9797
Satoshi Konno <skonno@cybergarage.org>
2021-11-23 07:00:40 +0900

    Update typing hints for Python 3.8

--- uecho/controller.py
+++ uecho/controller.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import time
-from typing import Any, Union, List
+from typing import Any, Union, List, Tuple
 
 from uecho.object import Object
 
@@ -121,7 +121,7 @@ class Controller(Observer):
         """
         return self.__node.announce_message(msg)
 
-    def send_message(self, msg: Message, addr: Union[tuple[str, int], str, RemoteNode]):
+    def send_message(self, msg: Message, addr: Union[Tuple[str, int], str, RemoteNode]):
         """Posts a unicast message to the specified node asynchronously.
 
             Args:
@@ -141,7 +141,7 @@ class Controller(Observer):
         msg = Controller.__SearchMessage()
         return self.announce_message(msg)
 
-    def post_message(self, msg: Message, addr: Union[tuple[str, int], str, RemoteNode]):
+    def post_message(self, msg: Message, addr: Union[Tuple[str, int], str, RemoteNode]):
         """Posts a unicast message to the specified node and return the response message synchronously.
 
             Args:
