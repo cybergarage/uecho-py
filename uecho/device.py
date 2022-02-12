49609abb800be04c04497865586bea961b4d22ba
Satoshi Konno <skonno@cybergarage.org>
2022-02-13 02:01:18 +0900

    Update Device::message_received()

--- uecho/device.py
+++ uecho/device.py
@@ -13,12 +13,12 @@
 # limitations under the License.
 
 import abc
-from typing import Any, Union, Tuple
+from typing import Any, Union, Tuple, List
 
 from .object import Object
 from .property import Property
-from .remote_node import RemoteNode
 from .std import Database
+from .message import Message
 
 
 class DeviceListener(metaclass=abc.ABCMeta):
@@ -26,15 +26,22 @@ class DeviceListener(metaclass=abc.ABCMeta):
     """
 
     @abc.abstractmethod
-    def property_requested(self, node: RemoteNode, obj: Object, prop: Property):
+    def property_read_requested(self, prop: Property):
+        pass
+
+    @abc.abstractmethod
+    def property_write_requested(self, prop: Property, data: bytes):
         pass
 
 
 class Device(Object):
 
+    __listeners: List[DeviceListener]
+
     def __init__(self, code: Union[int, Tuple[int, int], Tuple[int, int, int], Any] = None):
         super().__init__()
         self.set_code(code)
+        self.__listeners = []
 
     def set_code(self, code: Union[int, Tuple[int, int], Tuple[int, int, int], Any]) -> bool:
         """Sets the spcecified code as the object code.
@@ -53,3 +60,17 @@ class Device(Object):
             self._set_object_properties(std_obj)
 
         return True
+
+    def message_received(self, msg: Message):
+        if not isinstance(msg, Message):
+            return
+
+        for msg_prop in msg.properties:
+            prop = self.get_property(msg_prop.code)
+            if prop is None:
+                continue
+            for listener in self.__listeners:
+                if msg.is_read_request():
+                    listener.property_read_requested(self, prop)
+                elif msg.is_write_request():
+                    listener.property_write_requested(self, prop, msg_prop.data)
