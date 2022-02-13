c96eb5b3f75b251c030561499e6a255d648801c0
Satoshi Konno <skonno@cybergarage.org>
2022-02-13 12:03:06 +0900

    Move is_*() from Message to protocol.ESV

--- uecho/protocol/esv.py
+++ uecho/protocol/esv.py
@@ -37,3 +37,102 @@ class ESV(object):
 
     def __init__(self, code=UNKNOWN):
         self.ESV = code
+
+    def is_write_request(self) -> bool:
+        """ Checks the ESV whether it is classified as a write request type.
+
+        Returns:
+            bool: True whether the specified code is a write request type, otherwise False.
+        """
+        if self.ESV == ESV.WRITE_REQUEST:
+            return True
+        if self.ESV == ESV.WRITE_REQUEST_RESPONSE_REQUIRED:
+            return True
+        if self.ESV == ESV.WRITE_READ_REQUEST:
+            return True
+        return False
+
+    def is_read_request(self) -> bool:
+        """ Checks the ESV whether it is classified as a read request type.
+
+        Returns:
+            bool: True whether the specified code is a read request type, otherwise False.
+        """
+        if self.ESV == ESV.WRITE_REQUEST:
+            return True
+        if self.ESV == ESV.WRITE_READ_REQUEST:
+            return True
+        return False
+
+    def is_notification_request(self) -> bool:
+        """ Checks the ESV whether it is classified as a notification notification request type.
+
+        Returns:
+            bool: True whether the specified code is a notification request type, otherwise False.
+        """
+        if self.ESV == ESV.NOTIFICATION_REQUEST:
+            return True
+        return False
+
+    def is_write_response(self) -> bool:
+        """ Checks the ESV whether it is classified as a write response type.
+
+        Returns:
+            bool: True whether the specified code is a write response type, otherwise False.
+        """
+        if self.ESV == ESV.WRITE_RESPONSE:
+            return True
+        if self.ESV == ESV.WRITE_READ_RESPONSE:
+            return True
+        return False
+
+    def is_read_response(self) -> bool:
+        """ Checks the ESV whether it is classified as a read response type.
+
+        Returns:
+            bool: True whether the specified code is a read response type, otherwise False.
+        """
+        if self.ESV == ESV.READ_RESPONSE:
+            return True
+        if self.ESV == ESV.WRITE_READ_RESPONSE:
+            return True
+        return False
+
+    def is_notification(self) -> bool:
+        """ Checks the ESV whether it is classified as a notification type.
+
+        Returns:
+            bool: True whether the specified code is a notification type, otherwise False.
+        """
+        if self.ESV == ESV.NOTIFICATION:
+            return True
+        if self.ESV == ESV.NOTIFICATION_RESPONSE_REQUIRED:
+            return True
+        return False
+
+    def is_notification_response(self) -> bool:
+        """ Checks the ESV whether it is classified as a notification response type.
+        Returns:
+            bool: True whether the specified code is a notification response type, otherwise False.
+        """
+        if self.ESV == ESV.NOTIFICATION_RESPONSE:
+            return True
+        return False
+
+    def is_response_required(self) -> bool:
+        """ Checks the ESV whether it is classified as a response required type.
+
+        Returns:
+            bool: True whether the ESV requires the response, otherwise False.
+        """
+        if self.ESV == ESV.WRITE_REQUEST:
+            return True
+        if self.ESV == ESV.NOTIFICATION_REQUEST:
+            return True
+        if self.ESV == ESV.WRITE_READ_REQUEST:
+            return True
+        if self.ESV == ESV.WRITE_REQUEST_RESPONSE_REQUIRED:
+            return True
+        if self.ESV == ESV.NOTIFICATION_RESPONSE_REQUIRED:
+            return True
+        return False
