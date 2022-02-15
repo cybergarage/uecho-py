83188bc8e2d973d37ef3e1b44622298ed9ae0590
Satoshi Konno <skonno@cybergarage.org>
2022-02-15 17:59:12 +0900

    Update Message::parse_bytes()

--- test/protocol/message_test.py
+++ test/protocol/message_test.py
@@ -90,3 +90,81 @@ def test_parse_message():
     msg = Message()
     assert msg.parse_bytes(msg_bytes)
     assert_test_msg(msg)
+
+
+def test_parse_read_write_message():
+
+    tid = 100
+    seoj = 0x0A0B0C
+    deoj = 0x0D0E0F
+    opc = 3
+
+    def generate_test_msg_bytes():
+        tid_bytes = Bytes.from_int(tid, 2)
+        seoj_bytes = Bytes.from_int(seoj, 3)
+        deoj_bytes = Bytes.from_int(deoj, 3)
+
+        msg_bytes = bytearray([
+            Message.EHD1_ECHONET,
+            Message.EHD2_FORMAT1,
+            tid_bytes[0],
+            tid_bytes[1],
+            seoj_bytes[0],
+            seoj_bytes[1],
+            seoj_bytes[2],
+            deoj_bytes[0],
+            deoj_bytes[1],
+            deoj_bytes[2],
+            ESV.WRITE_READ_REQUEST,
+            opc,
+            1,
+            1,
+            0x41,    # a
+            2,
+            2,
+            0x41,    # a
+            0x42,    # b
+            3,
+            3,
+            0x41,    # a
+            0x42,    # b
+            0x43,    # c
+            opc,
+            1,
+            1,
+            0x41,    # a
+            2,
+            2,
+            0x41,    # a
+            0x42,    # b
+            3,
+            3,
+            0x41,    # a
+            0x42,    # b
+            0x43,    # c
+        ])
+
+        return msg_bytes
+
+    def assert_test_msg(msg):
+        assert msg.TID == tid
+        assert msg.SEOJ == seoj
+        assert msg.DEOJ == deoj
+        assert msg.ESV == ESV.WRITE_READ_REQUEST
+        # assert msg.OPC == opc
+        # assert len(msg.set_properties) == opc
+        # for n in range(opc):
+        #     prop = msg.properties[n]
+        #     assert prop.code == (n + 1)
+        #     assert len(prop.data) == (n + 1)
+        #     for i in range(len(prop.data)):
+        #         assert prop.data[i] == (0x41 + i)
+
+    msg = Message()
+    assert msg.parse_bytes(generate_test_msg_bytes())
+    assert_test_msg(msg)
+
+    # msg_bytes = msg.to_bytes()
+    # msg = Message()
+    # assert msg.parse_bytes(msg_bytes)
+    # assert_test_msg(msg)
