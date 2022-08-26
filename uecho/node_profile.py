cdcbc20070e157ea12d39a5ea77f4bb10430e2b2
Satoshi Konno <skonno@cybergarage.org>
2022-08-26 09:41:24 +0900

    Add NodeProile::__set_property_map_property() to support description format 2

--- uecho/node_profile.py
+++ uecho/node_profile.py
@@ -60,6 +60,10 @@ class NodeProfile(Profile):
     NOT_BOOTING = 0x31
     LOWER_COMMUNICATION_LAYER_PROTOCOL_TYPE = 0xFE
 
+    PROPERTYMAP_FORMAT1_MAX_SIZE = 15
+    PROPERTYMAP_FORMAT2_MAP_SIZE = 16
+    PROPERTYMAP_FORMAT_MAX_SIZE  = PROPERTYMAP_FORMAT2_MAP_SIZE + 1
+
     def __init__(self):
         super().__init__(NodeProfile.CODE)
         std_obj = Database().get_object(NodeProfile.CODE)
@@ -146,9 +150,28 @@ class NodeProfile(Profile):
         map_bytes = bytearray(bytes([len(prop_map)]))
         for prop_code in prop_map:
             map_bytes.extend(bytes([prop_code]))
-        if not self.set_property_data(code, map_bytes):
-            return False
-        return True
+        
+        # Description Format 1
+
+        if len(prop_map) <= NodeProfile.PROPERTYMAP_FORMAT1_MAX_SIZE:
+            map_bytes = bytearray(bytes([len(prop_map)]))
+            for prop_code in prop_map:
+                map_bytes.extend(bytes([prop_code]))
+            return self.set_property_data(code, map_bytes)
+
+        # Description Format 2
+
+        prop_map_codes = [0] * NodeProfile.PROPERTYMAP_FORMAT2_MAP_SIZE
+        for prop_code in prop_map:
+            # 0 <= propCodeIdx <= 15
+            prop_code_idx = ((prop_code - Property.CODE_MIN) & 0x0F)
+            # 0 <= propCodeIdx <= 7
+            prop_code_bit = ((((prop_code - Property.CODE_MIN) & 0xF0) >> 4) & 0x0F)
+            prop_map_codes[prop_code_idx] |= ((0x01 << prop_code_bit) & 0x0F)
+        map_bytes = bytearray(bytes([NodeProfile.PROPERTYMAP_FORMAT2_MAP_SIZE]))
+        for prop_map_code in prop_map_codes:
+            map_bytes.extend(bytes([prop_map_code]))
+        return self.set_property_data(code, map_bytes)
 
     def __update_property_map_properties(self, objs: List[Object]) -> bool:
         anno_list = []
